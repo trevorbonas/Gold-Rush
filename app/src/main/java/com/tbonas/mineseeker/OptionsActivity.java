33f21d8e15f4347728f2407650d26e8417164d75
trevorbonas <trevorbonas@gmail.com>
2020-10-15 16:38:52 -0700

    Add rough options activity

---
+++ app/src/main/java/com/tbonas/mineseeker/OptionsActivity.java
@@ -0,0 +1,87 @@
+package com.tbonas.mineseeker;
+
+import android.os.Bundle;
+
+import com.tbonas.mineseeker.model.MineField;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class OptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        setupRadioButtons();
+    }
+
+    private void setupRadioButtons() {
+        final MineField field = MineField.getInstance();
+        RadioGroup dimensionGroup = (RadioGroup)findViewById(R.id.b_size_group);
+        String[] dimensions = getResources().getStringArray(R.array.board_sizes);
+
+        final RadioGroup numMinesGroup = (RadioGroup)findViewById(R.id.ores_group);
+        int[] numMines = getResources().getIntArray(R.array.mine_num_options);
+
+        for (int i = 0; i < dimensions.length; i++) {
+            final String dimension = dimensions[i];
+
+            RadioButton button = new RadioButton(this);
+            button.setText(dimension);
+            dimensionGroup.addView(button);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String[] strings = dimension.split(" X ");
+                    int columns = Integer.parseInt(strings[0]);
+                    int rows = Integer.parseInt(strings[1]);
+
+                    field.resize(columns, rows);
+                }
+            });
+
+        }
+
+        for (int i = 0; i < numMines.length; i++) {
+            final int number = numMines[i];
+
+            final RadioButton button = new RadioButton(this);
+            button.setText("" + number);
+
+            numMinesGroup.addView(button);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (number > (field.getColumns() * field.getRows()) ) {
+                        Toast.makeText(OptionsActivity.this,
+                                "Number of ores greater than dimensions of mine",
+                                Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        field.setNumMines(number);
+                    }
+                }
+            });
+
+        }
+
+
+
+
+    }
+}
