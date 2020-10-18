19c5f21c130eded094c0a81fcb9793111067fb8e
trevorbonas <trevorbonas@gmail.com>
2020-10-17 21:12:28 -0700

    Added course hyperlink

--- app/src/main/java/com/tbonas/mineseeker/HelpActivity.java
+++ app/src/main/java/com/tbonas/mineseeker/HelpActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.Html;
 import android.view.View;
 import android.widget.TextView;
 
@@ -30,5 +31,15 @@ public class HelpActivity extends AppCompatActivity {
 
         TextView helpText = findViewById(R.id.helpContent);
         helpText.setText(R.string.helpText);
+
+        TextView courseTitle = findViewById(R.id.courseTitle);
+        courseTitle.setText(R.string.courseTitle);
+        courseTitle.setTextColor(getResources().getColor(R.color.solid_white,
+                this.getTheme()));
+
+        TextView web = findViewById(R.id.courseLink);
+        web.setText(Html.fromHtml(getString(R.string.website), Html.FROM_HTML_MODE_COMPACT));
+        web.setTextColor(getResources().getColor(R.color.solid_white,
+                this.getTheme()));
     }
 }
